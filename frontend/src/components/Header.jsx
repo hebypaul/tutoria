372ed7c031dcb6335ac899ec297ec58cb2cff53b
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-10 10:42:30 +0530

    added Hero section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
   <header class= "text-gray-600 body-font">
-    <div class="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <div class=" container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <>
+      <Hero />
+      
+    </>
   )
 }
 
