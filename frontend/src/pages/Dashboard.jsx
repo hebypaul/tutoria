21d328458e50440ffb647002357b25a0530d6a19
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-10 16:52:29 +0530

    Initial Home Page Design

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Cards from '../components/Cards'
+import Features from '../components/Features'
 import Hero from '../components/Hero'
+import Stats from '../components/Stats'
+import Testimonial from '../components/Testimonial'
 const Dashboard = () => {
   return (
     <>
       <Hero />
-      
+      <Cards />
+      <Features />
+      <Testimonial />
     </>
   )
 }
