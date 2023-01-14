3bc00ec0816aaf713e454b77584613d9d60701a4
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-14 17:50:26 +0530

    Edited login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <section class="text-gray-600 body-font">      
-      <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
-        <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:m-auto w-full mt-10 md:mt-0">
-          <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
-          <div class="relative mb-4">
-            <label for="full-name" class="leading-7 text-sm text-gray-600">Full Name</label>
-            <input type="text" id="full-name" name="full-name" class="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+    <section className="text-gray-600 body-font">      
+      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:m-auto w-full mt-10 md:mt-0">
+          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Login</h2>
+          <div className="relative mb-4">
+            <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+            <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
-          <div class="relative mb-4">
-            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <div className="relative mb-4">
+            <label for="full-name" className="leading-7 text-sm text-gray-600">Password</label>
+            <input type="password" id="password" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
-          <button class="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
-          <p class="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+          <button className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Login</button>
+          <p className="text-xs text-gray-500 mt-3">Click here to retain password if forgotten</p>
         </div>
       </div>
     </section>
