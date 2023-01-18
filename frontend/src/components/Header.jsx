54d5dc5602655ffce1d432cb832a0efce791bef0
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-18 16:52:29 +0530

    Added SignUp and login button on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,17 +10,14 @@ const Header = () => {
         </svg>
         <span class="ml-3 text-xl">Tutoria</span>
       </a>
-      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nav class="md:m-auto flex flex-wrap items-center text-base justify-center">
         <a class="mr-5 hover:text-gray-900" href="/courses">Explore Courses</a>
         <a class="mr-5 hover:text-gray-900">Course Creation</a>
         <a class="mr-5 hover:text-gray-900" href="/about">About</a>
         <a class="mr-5 hover:text-gray-900" href="/contact">Contact Us</a>
       </nav>
-      <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </button>
+      <a href="register"><button class="inline-flex items-center text-white bg-green-500 border-0 py-1 px-3 focus:outline-none hover:bg-green-600 rounded text-base mt-4 md:mt-0">Sign Up</button></a>
+      <a href="login"><button class="ml-4 inline-flex items-center bg-gray-100 border-0  py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login</button></a>
     </div>
   </header>
   )
