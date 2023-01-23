90a7a547751ab6b23a7a62010b9c34da68283da5
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-23 14:53:44 +0530

    User controller functions

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', true);
+
 const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI)
