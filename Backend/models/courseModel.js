1e06457bbd10783796b8459da9ba2b7bc732d848
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-26 19:22:21 +0530

    Updated course model

--- Backend/models/courseModel.js
+++ Backend/models/courseModel.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 
 const courseSchema = mongoose.Schema({
-    name:{
+    course_name:{
         type: String,
         required:[true,"Please add a course Name"]
-    }
+    },
+    course_details: String,
+    author: String,
+    category: String,
+    sub_category: String,
+    course_img: { type: String, default:""},
+    author_img: {type: String, default: ""},
 },{
     timestamps:true,
 })
