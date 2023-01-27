153489255e66c0fa79ae926da851536c5f481ca7
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-27 22:27:10 +0530

    Added sections to course model

---
+++ Backend/models/VideoModel.js
@@ -0,0 +1,6 @@
+const videoSchema = new Schema({
+    video_url: { type: String, required: true },
+    video_title: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Video', videoSchema);
--- Backend/models/courseModel.js
+++ Backend/models/courseModel.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose')
 
+const sectionSchema = new Schema({
+    section_name: { type: String, required: true },
+    section_text: { type: String, required: true },
+    video_ids: [{ type: Schema.Types.ObjectId, ref: "Video" }],
+    quiz_ids: [{ type: Schema.Types.ObjectId, ref: "Quiz" }],
+});
+
+
 const courseSchema = mongoose.Schema({
     course_name:{
         type: String,
         required:[true,"Please add a course Name"]
     },
-    course_details: String,
-    author: String,
-    category: String,
-    sub_category: String,
-    course_img: { type: String, default:""},
-    author_img: {type: String, default: ""},
+    course_details: { type: String, required: true },
+    author: { type: String, required: true },
+    level: { type: String, required: true },
+    sections: [sectionSchema],
+    category: { type: String, required: true },
+    course_img: { type: String, default: "" },
+    author_img: { type: String, default: "" },
 },{
     timestamps:true,
 })
