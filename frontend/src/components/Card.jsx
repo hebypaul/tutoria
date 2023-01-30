e860c6e565777dd2b1e49b4230344db3e37d343c
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-30 19:30:43 +0530

    Added card component

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+            <div class="bg-gray-100 p-6 rounded-lg">
+                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
+                <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3>
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+            </div>
+        </div>
+  )
+}
+
+export default Card
--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from './Card'
 
 const Cards = () => {
   return (
@@ -11,38 +12,10 @@ const Cards = () => {
             </div>
             </div>
             <div class="flex flex-wrap -m-4">
-            <div class="xl:w-1/4 md:w-1/2 p-4">
-                <div class="bg-gray-100 p-6 rounded-lg">
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-                <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3>
-                <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
-            </div>
-            <div class="xl:w-1/4 md:w-1/2 p-4">
-                <div class="bg-gray-100 p-6 rounded-lg">
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
-                <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3>
-                <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
-            </div>
-            <div class="xl:w-1/4 md:w-1/2 p-4">
-                <div class="bg-gray-100 p-6 rounded-lg">
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content" />
-                <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3>
-                <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
-            </div>
-            <div class="xl:w-1/4 md:w-1/2 p-4">
-                <div class="bg-gray-100 p-6 rounded-lg">
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content" />
-                <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3>
-                <h2 class="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
-                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
-            </div>
+                <Card/>
+                <Card />
+                <Card />
+                <Card />
             </div>
         </div>
     </section>
