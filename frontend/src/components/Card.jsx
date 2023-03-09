261c2e9411fe12c839568bf51e7de55dfc9d3512
Heby T Paul <hebytpaul1111@gmail.com>
2023-03-10 03:47:25 +0530

    Fixed Course List spacing

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({data}) => {
           <div class="p-2">
               <div class="bg-gray-100 p-6 rounded-lg">
                   <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
-                  <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3>
+                  <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">{data.author}</h3>
                   <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{data.course_name}</h2>
                   <p class="leading-relaxed text-base ">{data.course_details}</p>
               </div>
--- frontend/src/components/CourseList.jsx
+++ frontend/src/components/CourseList.jsx
@@ -4,9 +4,9 @@ import {Link } from 'react-router-dom';
 const CourseList = ({data}) => {
   return (
     <Link to={`/courses/${data._id}`}>
-        <div className="py-8 flex flex-wrap md:flex-nowrap">
-            <div className="md:w-64 md:mb-0 mb-6  pr-6 flex-shrink-0 flex flex-col">
-            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
+        <div className="px-2 py-4 border-b-2 border-gray-300 flex flex-wrap md:flex-nowrap ">
+            <div className="md:w-64 md:mb-0  pr-6 flex-shrink-0 flex flex-col">
+            <img class="h-40 rounded w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="content" />
             </div>
             <div className="md:flex-grow">
             <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">{data.course_name}</h2>
@@ -14,8 +14,8 @@ const CourseList = ({data}) => {
             <div className="my-2">
                 <h3 class="tracking-widest text-green-500 text-xs font-medium title-font">{data.author}</h3>
                 <h3 class="tracking-widest text-gray-500 text-xs font-medium title-font">
-                    <span class="inline-block py-1 px-2 mr-1 my-1 rounded-md bg-gray-200 text-xs font-medium tracking-widest">{data.level} level </span>
-                    <span class="inline-block py-1 px-2 mr-1 my-1 rounded-md bg-gray-200 text-xs font-medium tracking-widest">{data.category} </span>
+                    <span class="inline-block p-1  mr-1 my-1 rounded-md bg-gray-100 font-medium tracking-widest">{data.level}</span>
+                    <span class="inline-block p-1  mr-1 my-1 rounded-md bg-gray-100 font-medium tracking-widest">{data.category}</span>
                 </h3>
             </div>
             </div>
