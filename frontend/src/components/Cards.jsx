9c639ac9f371f4182882d64e43c7bbbfa68f9f30
Heby T Paul <hebytpaul1111@gmail.com>
2023-04-01 02:52:25 +0530

    Course Lists design modification

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import CardCarousel from './CardCarousel'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
 
 const Cards = () => {
   return (
@@ -9,7 +8,7 @@ const Cards = () => {
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap w-full mb-20">
             <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">All courses</h1>
                 <div class="h-1 w-20 bg-green-500 rounded"></div>
             </div>
             </div>
--- frontend/src/components/CoursesLists.jsx
+++ frontend/src/components/CoursesLists.jsx
@@ -11,19 +11,24 @@ const CoursesLists = () => {
       setCourses(res);
     })
   },[])
-  
+
   return (
-    <div className="container px-5 py-24 mx-auto">
-    <div className="-my-8 divide-y-2 divide-gray-100">
+    <div className="container px-5 pb-24 mx-auto">
+      <div class="flex flex-wrap w-full mb-24">
+        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">All Courses</h1>
+          <div class="h-1 w-20 bg-green-500 rounded"></div>
+        </div>
+      </div>   
+      <div className="-my-8 divide-y-2 divide-gray-100">
+      {courses?.map((course)=>(
+            <CourseList data={course}/>
+      ))
+      }
 
-    {courses?.map((course)=>(
-          <CourseList data={course}/>
-    ))
-    }
 
-
+      </div>
     </div>
-  </div>
   )
 }
 
