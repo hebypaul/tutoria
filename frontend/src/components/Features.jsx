b0cb1e6dfaa4d30a5745c768341fab4b10f05301
heby  T Paul <hebytpaul1111@gmail.com>
2024-03-03 23:24:12 +0530

    typo-fix 3

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -22,7 +22,7 @@ const Features = () => {
                 <div className="flex-grow">
                     <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
                     <a className="mt-3 text-green-500 inline-flex items-center">Learn More
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
                     </a>
